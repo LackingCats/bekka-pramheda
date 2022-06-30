b3513975961da9126813b3bdb51b01c46fb34a72
lackingcats <jackson.marsden6@gmail.com>
2022-06-30 10:48:49 +1000

    added FloaterBlock

--- src/main/java/net/theelo/bekka/block/ModBlocks.java
+++ src/main/java/net/theelo/bekka/block/ModBlocks.java
@@ -12,6 +12,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.theelo.bekka.BekkaPramheda;
+import net.theelo.bekka.block.custom.FloaterBlock;
 import net.theelo.bekka.lib0.ModItemGroup;
 
 public class ModBlocks {
@@ -42,6 +43,9 @@ public class ModBlocks {
             new OreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(8f).luminance(10),
                     UniformIntProvider.create(5, 10)),
                             ModItemGroup.TANZANITE);
+    public static final Block FLOATER = registerBlock("floater",
+            new FloaterBlock(FabricBlockSettings.of(Material.METAL).requiresTool().strength(85f).luminance(15)),
+                    ModItemGroup.TANZANITE);
 // public static final Block ENDSTONE_TANZANITE_ORE = registerBlock("endstone_tanzanite_ore",
 //         new OreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(8f).luminance(8),
 //                 UniformIntProvider.create(7, 14)),
---
+++ src/main/java/net/theelo/bekka/block/custom/FloaterBlock.java
@@ -0,0 +1,40 @@
+package net.theelo.bekka.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class FloaterBlock extends Block {
+    public FloaterBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (entity instanceof LivingEntity livingEntity) {
+            livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 20*200, 8, false, false, false));
+        }
+        if (entity instanceof PlayerEntity player) {
+            player.sendMessage(new LiteralText("bye"), true);
+        }
+        super.onSteppedOn(world, pos, state, entity);
+    }
+
+
+    @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+        lightning.setPosition(player.getPos());
+        world.spawnEntity(lightning);
+        super.onBreak(world, pos, state, player);
+    }
+}
