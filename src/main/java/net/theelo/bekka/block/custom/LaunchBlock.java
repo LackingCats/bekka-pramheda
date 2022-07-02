1e2180ca2563f8bc0b7f2b09decd5502d9f33b4d
lackingcats <jackson.marsden6@gmail.com>
2022-07-02 16:50:48 +1000

    b1 -> b2 (also launch 🚀)

--- src/main/java/net/theelo/bekka/block/custom/LaunchBlock.java
+++ src/main/java/net/theelo/bekka/block/custom/LaunchBlock.java
@@ -4,10 +4,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
 public class LaunchBlock extends Block {
     public LaunchBlock(Settings settings) {
@@ -15,16 +19,45 @@ public class LaunchBlock extends Block {
     }
 
     @Override
-    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-
-        if(!world.isClient()){
-            if (entity instanceof LivingEntity livingEntity) {
-                livingEntity.addVelocity(livingEntity.getX(), livingEntity.getY() + 480, livingEntity.getZ());
-            }
-            if (entity instanceof PlayerEntity player) {
-                player.sendMessage(new LiteralText("boing"), true);
-            }
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, @NotNull Entity entity, float fallDistance) {
+        if (entity.bypassesLandingEffects()) {
+            super.onLandedUpon(world, state, pos, entity, fallDistance);
+        } else {
+            entity.handleFallDamage(fallDistance, 0.0f, DamageSource.FALL);
         }
+    }
+
+    @Override
+    public void onEntityLand(BlockView world, @NotNull Entity entity) {
+        if (entity.bypassesLandingEffects()) {
+            super.onEntityLand(world, entity);
+        } else {
+            this.bounce(entity);
+        }
+    }
+
+    private void bounce(@NotNull Entity entity) {
+        Vec3d vec3d = entity.getVelocity();
+        if (vec3d.y < 0.0) {
+            double d = entity instanceof LivingEntity ? 14.0 : 13.8;
+            entity.setVelocity(vec3d.x, -vec3d.y * d, vec3d.z);
+        }
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, @NotNull Entity entity) {
+        double d = Math.abs(entity.getVelocity().y);
+        if (d < 0.1 && !entity.bypassesSteppingEffects()) {
+            double e = 13.2 + d * 4.8;
+            entity.setVelocity(entity.getVelocity().multiply(e, 13.0, e));
+        }
+        if(entity instanceof LivingEntity livingEntity) {
+            livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*15, 25, false, false, false));
+        }
+
+
         super.onSteppedOn(world, pos, state, entity);
     }
 }
+
+
